71023fcab01d95179eee2207c65c1cc20522101a
Valentin <contact@valani.dev>
2025-12-10 14:42:41 +0100

    Add custom COC type

---
+++ types/clash.ts
@@ -0,0 +1,10 @@
+// Data Retrieved From In Game Export
+export type ClashItem = {
+  data: number
+  lvl?: number
+  cnt?: number
+  weapon?: number
+  timer?: number
+  gear_up?: number
+  helper_cooldown?: number
+}
